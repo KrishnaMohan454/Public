759c379a1aadd08b6cac9a7818472e0fca393bf5
Krishna Mohan <krishnamohan@Krishnas-MacBook-Pro.local>
2020-06-09 17:35:11 +0530

    added framework

---
+++ ExampleFramework.framework/Headers/ExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  ExampleFramework.h
+//  ExampleFramework
+//
+//  Created by Krishna Mohan on 09/06/20.
+//  Copyright © 2020 IDM014. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ExampleFramework/ModelClass.h>
+//! Project version number for ExampleFramework.
+FOUNDATION_EXPORT double ExampleFrameworkVersionNumber;
+
+//! Project version string for ExampleFramework.
+FOUNDATION_EXPORT const unsigned char ExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFramework/PublicHeader.h>
+
+
---
+++ ExampleFramework.framework/Headers/ModelClass.h
@@ -0,0 +1,19 @@
+//
+//  ModelClass.h
+//  ExampleFramework
+//
+//  Created by Krishna Mohan on 09/06/20.
+//  Copyright © 2020 IDM014. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface ModelClass : NSObject
+
+-(void)showFrameworkNameInApp;
+
+@end
+
+
